b18188583fecad39c38d9c1be961d7de278fe7ff
Ahmed Magdy ELzayady <ahmedelzayady894@gmail.com>
2022-08-08 01:43:04 +0200

    vv
    dwd

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int *ptr;
+  double *ptr2;
+  printf("size of integer is : %i\n", sizeof(ptr));
+  printf("size of double is : %i", sizeof(ptr2));
+  return 0;
+}
